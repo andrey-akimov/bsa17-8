07a9b719afc4197214bb9a5c68265e7ab8f18e43
Andrey <rojer9999@gmail.com>
2017-10-09 23:14:49 +0300

    styles fixed

--- src/js/main.js
+++ src/js/main.js
@@ -1,11 +1,10 @@
-import 'babel-polyfill'
-import React, { Component } from 'react'
-import { render } from 'react-dom'
-import './components/style.scss'
-import App from './components/app.js'
-import UsersList from './components/userslist.js'
-import User from './components/user.js'
-import AddUser from './components/adduser.js'
-
+import 'babel-polyfill';
+import React, { Component } from 'react';
+import { render } from 'react-dom';
+import './components/style.scss';
+import App from './components/app.js';
+import UsersList from './components/userslist.js';
+import User from './components/user.js';
+import AddUser from './components/adduser.js';
 
 render(<App />, document.getElementById('app'));
